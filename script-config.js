6a952f8c97c41b65b77e67c341de7d0eaf073556
pinktabit <pinktabit@users.noreply.github.com>
2026-05-10 07:59:15 +0900

    Connect RSVP to Apps Script endpoint

--- script-config.js
+++ script-config.js
@@ -1,3 +1,3 @@
 window.INVITATION_CONFIG = {
-  appsScriptUrl: "",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbwxn6E3xe5124mrlXGSGzMqXgstijHSXmTeLHJLwdT-FF4z10KOM2IX_E_W5giho7O2/exec",
 };
